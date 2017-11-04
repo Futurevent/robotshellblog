75bf0e5931c6cc67a092262fe7cf680b2312791a
Futurevent <zj2012zy@163.com>
2017-11-05 01:03:49 +0800

    modify deploy.sh

--- futurevent.github.io/deploy.sh
+++ futurevent.github.io/deploy.sh
@@ -1,7 +1,9 @@
 hexo generate
-cp -r public/* ../../futurevent
 cd ../../futurevent
 git pull
+cd -
+cp -r public/* ../../futurevent
+cd ../../futurevent
 git add -A .
 git commit -m "update blog"
 git push origin master
